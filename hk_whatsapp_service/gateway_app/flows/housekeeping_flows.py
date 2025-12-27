ec7ccdcafe87ad3a2f7b55ad69695a5e59505bda
Andrés <aortiz.osadey@gmail.com>
2025-12-27 11:02:44 -0300

    Eliminamos handler.py

--- hk_whatsapp_service/gateway_app/flows/housekeeping_flows.py
+++ hk_whatsapp_service/gateway_app/flows/housekeeping_flows.py
@@ -1,4 +1,4 @@
-from .housekeeping.handler import handle_hk_message, hk_check_reminder
+from .housekeeping.orchestrator import handle_hk_message
+from .housekeeping.reminders import hk_check_reminder
 
 __all__ = ["handle_hk_message", "hk_check_reminder"]
-
